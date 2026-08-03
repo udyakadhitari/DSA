ea518d6d74120998580a9c174b681870b8025f58
udyakadhitari <udyakadhitari@gmail.com>
2026-08-04 02:12:09 +0530

    Time: 4 ms (18.93%) | Memory: 28.4 MB (13.06%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+
+        for(int i =0 ; i < nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+
+        for(auto i : mpp){
+            if(i.second > (nums.size()/2)) {
+                return i.first;
+            }
+        }
+
+        return -1;
+        
+    }
+};
