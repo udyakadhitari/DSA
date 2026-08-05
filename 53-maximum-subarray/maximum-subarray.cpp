b5f9b44dd28de6416b7b1cbae0802e6989a14050
udyakadhitari <udyakadhitari@gmail.com>
2026-08-06 00:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (98.84%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -9,9 +9,7 @@ public:
         for(int i =0; i<nums.size(); i++){
             sum += nums[i];
 
-            if(sum>maxi){
-                maxi = sum;
-            }
+            maxi = max(maxi,sum);
 
             if(sum<0){
                 sum = 0;
