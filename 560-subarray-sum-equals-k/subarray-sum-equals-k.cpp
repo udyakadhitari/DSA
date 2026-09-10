f642a9553f6f733d9e427c86d91f95ddd28073cb
udyakadhitari <udyakadhitari@gmail.com>
2026-09-10 14:08:48 +0530

    Time: 79 ms (26.12%) | Memory: 53.8 MB (6.43%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -10,8 +10,7 @@ public:
 
         for(int i =0; i< nums.size(); i++){
             preSum += nums[i];
-            int remove = preSum - k;
-            cnt += mpp[remove];
+            cnt += mpp[ preSum - k];
             mpp[preSum] += 1;
 
 
